5b4f62f7269771d32d2c86bb9859c0ac17dbf276
Nilay R Gala <nilay.gala@in.ey.com>
2023-06-01 12:38:16 +0530

    Check for numbers added

--- MultiplyNum/index.js
+++ MultiplyNum/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, req) {
     context.log("Javascript HTTP trigger processed a request.");
 
-    if(req.query.num1 && req.query.num2){
+    if((req.query.num1 && req.query.num2)&&(!isNaN(req.query.num1) && !isNaN(req.query.num2))){
         context.res = {
             body: req.query.num1 * req.query.num2
         };
